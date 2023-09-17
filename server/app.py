d9a31b51baade6489fe1c1fd799a1e727c096772
Tong Tang <pichip22@hotmail.com>
2023-09-17 16:39:19 -0400

    completed

--- server/app.py
+++ server/app.py
@@ -17,11 +17,43 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
+        plants_dict = [plant.to_dict() for plant in plants]
+
+        response = make_response(jsonify(plants_dict),200)
+        return response
+    
+    def post(self):
+        plant = request.get_json()
+        new_plant = Plant(
+            name=plant['name'],
+            image=plant['image'],
+            price=plant['price'],
+        )
+
+        db.session.add(new_plant)
+        db.session.commit()
+
+        plant_dict = new_plant.to_dict()
+
+        response = make_response((plant_dict), 201)
+        
+        return response
+
+
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self,id):
+        plant = Plant.query.filter_by(id=id).first()
+        plant_dict = plant.to_dict()
+
+        response = make_response(jsonify(plant_dict),200)
+        return response
+    
+
+api.add_resource(Plants, '/plants')
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
